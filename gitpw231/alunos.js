e70fe180bfa00412c96a31da1764e4dbd9726943
Caetanocc <claudiomiro.silva2@etec.sp.gov.br>
2023-04-08 21:31:18 -0300

    atualizações. novos alunos

---
+++ gitpw231/alunos.js
@@ -0,0 +1,40 @@
+var alunos = 
+["ALUNO",
+'ANTONIO',   
+'AUGUSTO',  
+'BRUNO',    
+'CARLOS',   
+'CECILIA',  
+'DIEGO',    
+'ENZO',     
+'EVELYN',   
+'FELIPE',   
+'GEOVANNA', 
+'GUILHERME',
+'JESSICA',  
+'JOAO',     
+'LEONARDO', 
+'LUCCAS',   
+'MARCELO',  
+'MARIANA',  
+'MATHEUS',  
+'NICOLE',   
+'RAMON',    
+'RODRIGO',  
+'ULISSES',  
+'VINICIUS', 
+'YURI',     
+];
+
+var len = alunos.length;
+var text = '<table id="t01"> ';
+
+for (i=0; i < len; i++) {
+    text += "<tr><td>" + i + "</td>";
+    text += "<td>" + alunos[i] + "</td>";
+    text += '<td><img src="' + alunos[i] + '.gif" alt="' + alunos[i] + '" style="width:200px;height:100px;"></td>'
+    text += "</tr>";
+}
+
+text += "</table>";
+document.getElementById("lista").innerHTML = text;
